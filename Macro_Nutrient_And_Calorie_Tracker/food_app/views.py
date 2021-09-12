4f07fe54255a4fbedae38e5f9056eaf02a0747fa
Piotr-Zielinski-PZ <piotrzet00@outlook.com>
2021-09-12 14:52:10 +0200

    First commit

---
+++ Macro_Nutrient_And_Calorie_Tracker/food_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .models import Food, Consume
+
+
+def index(request):
+    if request.method=='POST':
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=consume)
+        consume.save()
+        food = Food.objects.all()
+    else:
+        food = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
+    return render(request, 'food_app/index.html', {'food':food, 'consumed_food':consumed_food,})
+
+def delete_consume(request,id):
+    consumed_food = Consume.objects.get(id=id)
+    if request.method=='POST':
+        consumed_food.delete()
+        return redirect('/')
+    return render(request, 'food_app/delete.html')
